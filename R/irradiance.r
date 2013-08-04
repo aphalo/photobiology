51c79125693b66ab7b408ffa0f57a19d930664fe
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2013-08-04 10:26:44 +0300

    Added argument to irradiance() for enabling the caching of multipliers.

--- R/irradiance.r
+++ R/irradiance.r
@@ -3,7 +3,8 @@
 #' This function gives the energy irradiance for a given
 #' waveband of a radiation spectrum.
 #'
-#' @usage irradiance(w.length, s.irrad, w.band=NULL, unit.out=NULL, unit.in="energy", check.spectrum=TRUE)
+#' @usage irradiance(w.length, s.irrad, w.band=NULL, unit.out=NULL, unit.in="energy", 
+#' check.spectrum=TRUE, use.cached.mult=FALSE)
 #' 
 #' @param w.length numeric array of wavelength (nm)
 #' @param s.irrad numeric array of spectral (energy) irradiances (W m-2 nm-1)
@@ -11,6 +12,7 @@
 #' @param unit.out character string with allowed values "energy", and "photon", or its alias "quantum"
 #' @param unit.in character string with allowed values "energy", and "photon", or its alias "quantum"
 #' @param check.spectrum logical indicating whether to sanity check input data, default is TRUE
+#' @param use.cached.mult logical indicating whether multiplier values should be cached between calls
 #' 
 #' @return a single numeric value with no change in scale factor: [W m-2 nm-1] -> [mol s-1 m-2]
 #' @keywords manip misc
@@ -20,7 +22,8 @@
 #' with(sun.data, irradiance(w.length, s.e.irrad, new_waveband(400,700), "photon"))
 
 irradiance <- 
-  function(w.length, s.irrad, w.band=NULL, unit.out=NULL, unit.in="energy", check.spectrum=TRUE){
+  function(w.length, s.irrad, w.band=NULL, unit.out=NULL, unit.in="energy", 
+           check.spectrum=TRUE, use.cached.mult=FALSE){
     # what output? seems safer to not have a default here
     if (is.null(unit.out)){
       warning("'unit.out' has no default value")
@@ -29,7 +32,9 @@ irradiance <-
     # make code a bit simpler further down
     if (unit.in=="quantum") {unit.in <- "photon"}
     # sanity check for spectral data and abort if check fails
-    if (check.spectrum && !check_spectrum(w.length, s.irrad)) return(NA)
+    if (check.spectrum && !check_spectrum(w.length, s.irrad)) {
+      return(NA)
+    } 
     # if the waveband is undefined then use all data
     if (is.null(w.band)){
       w.band <- new_waveband(min(w.length),max(w.length))
@@ -41,7 +46,8 @@ irradiance <-
       s.irrad <- new.data$s.irrad
     }
     # calculate the multipliers
-    mult <- calc_multipliers(w.length, w.band, unit.out, unit.in)
+    mult <- calc_multipliers(w.length=w.length, w.band=w.band, unit.out=unit.out, 
+                             unit.in=unit.in, use.cached.mult=use.cached.mult)
     
     # calculate weighted spectral irradiance
     irrad <- integrate_irradiance(w.length, s.irrad * mult)
--- test/test.calc.multipliers.r
+++ test/test.calc.multipliers.r
@@ -14,4 +14,26 @@ test.calc_multipliers <- function(w.band=new_waveband(400,700)) {
   print(ptm0)
   print(ptm1 - ptm0)
   print(ptm2 - ptm1)
+  ptm0 <- proc.time()
+  for (i in 1:10000){
+    with(sun.data, calc_multipliers(w.length, w.band,"photon", use.cached.mult=FALSE))  
+  }
+  ptm1 <- proc.time()
+  for (i in 1:10000){
+    with(sun.data, irradiance(w.length, s.e.irrad, w.band,"photon", check.spectrum=TRUE, use.cached.mult=FALSE))  
+  }
+  ptm1 <- proc.time()
+  for (i in 1:10000){
+    with(sun.data, irradiance(w.length, s.e.irrad, w.band,"photon", check.spectrum=FALSE, use.cached.mult=TRUE))  
+  }
+  ptm2 <- proc.time()
+  print(ptm0)
+  print(ptm1 - ptm0)
+  print(ptm2 - ptm1)
 }
+
+test.calc_multipliers()
+test.calc_multipliers(DNA.N())
+test.calc_multipliers(CIE())
+test.calc_multipliers(CIE(300))
+
