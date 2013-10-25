f4db0db4971939e5d11249b6c32739ba5c8a0866
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2013-10-25 19:59:24 +0300

    Deleted batch files.

--- build_photobiology.bat
+++
@@ -1,4 +0,0 @@
-set path=c:\Rtools\bin;c:\Rtools\perl\bin;c:\Rtools\gcc-4.6.3\bin;c:\R\bin;c:\Program Files\R\R-3.0.1\bin\i386
-cd C:/Users/%USERNAME%/Documents/Rpackages
-Rcmd.exe INSTALL --build --compile-both --library=C:/Users/%USERNAME%/Documents/R/win-library/3.0 photobiology_0.2.8.tar.gz
-cd photobiology
