c9a783dd3d85e180bed71a71f48b5120e82a35d6
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2013-12-26 18:34:51 +0200

    Added parameter to trim_tails to allow insertion of "hinges" to be disabled.

--- R/trim.tails.r
+++ R/trim.tails.r
@@ -5,12 +5,13 @@
 #' when the measured spectrum extends beyond the known emission
 #' spectrum of the measured light source.
 #' 
-#' @usage trim_tails(w.length, s.irrad, low.limit=min(w.length), high.limit=max(w.length), fill=NULL) 
+#' @usage trim_tails(w.length, s.irrad, low.limit=min(w.length), high.limit=max(w.length), use.hinges=TRUE, fill=NULL) 
 #' 
 #' @param w.length numeric array of wavelengths (nm)
 #' @param s.irrad numeric array of spectral irradiance values
 #' @param low.limit shortest wavelength to be kept (defaults to shortest w.length value)
 #' @param high.limit longets wavelength to be kept (defaults to longest w.length value)
+#' @param use.hinges logical, if TRUE (the default) 
 #' @param fill if fill==NULL then tails are deleted, otherwise tails or s.irrad are filled with the value of fill
 #' 
 #' @return a data.frame with variables \code{w.length} and \code{s.irrad}
@@ -22,12 +23,14 @@
 #' head(sun.data)
 #' head(with(sun.data, trim_tails(w.length, s.e.irrad, low.limit=300)))
 
-trim_tails <- function(w.length, s.irrad, low.limit=min(w.length), high.limit=max(w.length), fill=NULL)
+trim_tails <- function(w.length, s.irrad, low.limit=min(w.length), high.limit=max(w.length), use.hinges=TRUE, fill=NULL)
 {
   # insert hinges
-  new.data <- insert_hinges(w.length, s.irrad, c(low.limit, high.limit))
-  w.length <- new.data$w.length
-  s.irrad <- new.data$s.irrad
+  if (use.hinges) {
+    new.data <- insert_hinges(w.length, s.irrad, c(low.limit, high.limit))
+    w.length <- new.data$w.length
+    s.irrad <- new.data$s.irrad
+  }
   trimmed.selector <- w.length >= low.limit & w.length <= high.limit
   if (is.null(fill)) {
     return(data.frame(w.length=w.length[trimmed.selector], s.irrad=s.irrad[trimmed.selector]))
