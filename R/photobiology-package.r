96775c35fa3c15eb4a33bcd83f54b4984af6f880
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2016-01-03 11:03:19 +0200

    Preview as uploaded to repository.

--- R/photobiology-package.r
+++ R/photobiology-package.r
@@ -14,7 +14,7 @@
 #' \tabular{ll}{
 #' Package: \tab photobiology\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.9.1\cr
+#' Version: \tab 0.9.2\cr
 #' Date: \tab 2015-12-30\cr
 #' License: \tab GPL (>= 3.0)\cr
 #' URL: \tab \url{http://www.r4photobiology.info},\cr
