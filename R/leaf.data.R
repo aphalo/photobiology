7905b3656fff0ba15fb4302794d78a055d717190
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-03-20 10:33:49 -0300

    Add spectral reflectance data.

---
+++ R/leaf.data.R
@@ -0,0 +1,15 @@
+#' @title Green birch leaf reflectance.
+#'
+#' @description A dataset of spectral reflectance expressed as a fraction of one.
+#'
+#' @details \itemize{ \item w.length (nm) \item Rfr (0..1) }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A \code{reflector_spct} object with 226 rows and 2 variables
+#' @name green_leaf.spct
+#' @family Spectral data examples
+#' @references
+#' Aphalo, P. J. & Lehto, T. Effects of light quality on growth and N
+#' accumulation in birch seedlings Tree Physiology, 1997, 17, 125-132
+NULL
