2c32e92ecca95b7c2b4b61ae4dd97d7d83e222a9
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-12-18 13:25:25 +0200

    Fix docs.

--- R/beesxyz.data.r
+++ R/beesxyz.data.r
@@ -2,7 +2,9 @@
 #'
 #' @description A dataset containing wavelengths at a 5 nm interval (300 nm to
 #'   700 nm) and the corresponding x, y, and z chromaticity coordinates.
-#'   Original data from A chroma_spct object with variables as follows:
+#'   Original data from XXX.
+#'
+#'   A chroma_spct object with variables as follows:
 #'
 #' @details
 #' \itemize{
