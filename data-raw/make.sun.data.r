fcf894a39efc9fcca99a35a78da6868f6588d5c6
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2018-06-12 20:46:49 +0300

    Add script to resave data (optimised compression) and resAve all data objects.

--- data-raw/make.sun.data.r
+++ data-raw/make.sun.data.r
@@ -1,7 +1,10 @@
 library(photobiology)
 library(dplyr)
 library(lubridate)
-library(ggmap)
+
+geocode.kumpula <- data.frame(lat = 60.20911,
+                              lon = 24.96474,
+                              address = "Kumpula, Helsinki, FI")
 
 sun.midday.data <- read.table("data-raw/sun_20100622_midday.txt", col.names = c("time_min","w.length","s.e.irrad"))
 sun.midday.data$time_min <- NULL
@@ -13,7 +16,7 @@ sun.data <- as_data_frame(sun.midday.data)
 sun.spct <- as.source_spct(sun.data)
 sun.spct <- trim_spct(sun.spct, low.limit = 280, fill = 0, use.hinges = FALSE)
 setWhenMeasured(sun.spct, ymd_hms("2010-06-22 9:51:00"), tz = "UTC")
-setWhereMeasured(sun.spct, geocode("Kumpula, Helsinki, FI", "latlona"))
+setWhereMeasured(sun.spct, geocode.kumpula)
 setWhatMeasured(sun.spct, "sunlight, simulated")
 comment(sun.spct) <- "Simulated solar spectrum based on real weather conditions.\nData author: Dr. Anders Lindfors\nFinnish Meteorological Institute.\nSee help file for references."
 
@@ -28,7 +31,7 @@ sun.daily.data <- as_data_frame(sun.daily.data)
 sun.daily.spct <- as.source_spct(sun.daily.data, time.unit = "day")
 sun.daily.spct <- trim_spct(sun.daily.spct, low.limit = 280, fill = 0, use.hinges = FALSE)
 setWhenMeasured(sun.daily.spct, ymd("2012-06-01"), tz = "UTC")
-setWhereMeasured(sun.daily.spct, geocode("Kumpula, Helsinki, FI", "latlona"))
+setWhereMeasured(sun.daily.spct, geocode.kumpula)
 setWhatMeasured(sun.daily.spct, "sunlight, simulated")
 comment(sun.daily.spct) <- "Total daily spectral exposure estimated from hourly simulations of the solar spectrum\nbased real weather conditions.\nData author: Dr. Anders Lindfors\nFinnish Meteorological Institute.\nSee help file for references."
 
