2895d75828ad1ab147c9231a08937822264d177d
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-02-20 07:19:30 +0200

    http -> https in.om-attach.R

--- R/on-attach.R
+++ R/on-attach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("For news and documentation about the R for Photobiology packages, please, visit http://www.r4photobiology.info/")
+  packageStartupMessage("For news and documentation about the R for Photobiology packages, please, visit https://www.r4photobiology.info/")
 }
 
 .onLoad <- function(libname, pkgname) {
