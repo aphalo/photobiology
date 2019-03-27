1bd589c1ad6fb5bf21cab21e4234ddcbb604b21e
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-03-27 22:34:06 +0200

    Update dependency versions.

--- R/on-attach.R
+++ R/on-attach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("For news and documentation about the R for Photobiology packages, please, visit https://www.r4photobiology.info/")
+  packageStartupMessage("For news on the R for Photobiology packages, please, visit https://www.r4photobiology.info/")
 }
 
 .onLoad <- function(libname, pkgname) {
