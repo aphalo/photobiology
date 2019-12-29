28a6cdbfc96a0e65996d5e025b7f3b8d01163d93
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-12-30 01:56:32 +0200

    Add wl_thin() method
    This is only a first attempt at developing an efficient and flexible algorithm with little loss of information. Main aim is to make the data-only packages more efficient in storage use.

--- R/spct.utils.r
+++ R/spct.utils.r
@@ -11,9 +11,6 @@
 #' @return Utility used for its side effects, invisibly returns a character
 #'   vector with the names of the objects created.
 #'
-#' @note In its current implementation \code{uncollect()} will overwrite
-#' exisiting objects in the parent frame without warning. Use it with care.
-#'
 #' @export
 #'
 #' @examples
@@ -94,5 +91,102 @@ uncollect.generic_mspct <- function(x,
     assign(obj.names[member.name], x[[member.name]], envir = parent.frame(2))
   }
   invisible(unname(obj.names))
+}
+
+# thining of wavelengths --------------------------------------------------
+
+#' Thin the density of wavelength values
+#'
+#' Increase the wavelength step in stored spectral data in featureless
+#' regions to save storage space.
+#'
+#' @param x An R object
+#' @param ... additional named arguments passed down to \code{f}.
+#'
+#' @return An object of the same class as \code{x} but with a reduced density
+#' of wavelength values in those regions were slope is shallow and featureless.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' my.mscpt <- source_mspct(list(sun1 = sun.spct, sun2 = sun.spct))
+#' uncollect(my.mscpt)
+#' ls(pattern = "*.spct")
+#'
+#' @family experimental utility functions
+#'
+wl_thin <- function(x, ...) UseMethod("wl_thin")
+
+#' @describeIn wl_thin Default for generic function
+#'
+#' @export
+#'
+wl_thin.default <- function(x, ...) {
+  warning("'wl_thin()' is not defined for objects of class '", class(x)[1], "'.")
+  invisible(character())
+}
+
+#' @describeIn wl_thin
+#'
+#' @param max.wl.step numeric. Largest allowed wavelength difference between
+#'    adjacent spectral values.
+#' @param delta.var.max numeric in 0 to 1. Largest allowed relative difference
+#'    in spectral quantity betweem adjacent spectral values.
+#' @param variable character. Name of the variable conatining the spectral
+#'    data.
+#'
+#' @note Wavelengths are always stored expressed in nanometres in variable
+#'    \code{"w.length"}.
+#'
+#' @export
+#'
+wl_thin.generic_spct <- function(x,
+                                 max.wl.step = 2.0,
+                                 delta.var.max = 0.01,
+                                 variable,
+                                 ...) {
+  wl.stepsize <- wl_stepsize(x)
+  # make code simpler as range is fixed
+  x.norm <- normalize(x)
+  # collect peaks and valleys to ensure that they are not removed
+  peaks <- find_peaks(x.norm[[variable]], span = 21, strict = FALSE)
+  valleys <- find_peaks(-x.norm[[variable]], span = 21, strict = FALSE)
+  extremes <- peaks | valleys
+
+  # compute differences as proxi for slope
+  # using all values
+  wl.step1 <- x.norm[["w.length"]]
+  var.step1 <- x.norm[[variable]]
+  diff.wl.step1 <- diff(wl.step1)
+  diff.var.step1 <- diff(var.step1)
+  local.slope.step1 <- diff.var.step1 / diff.wl.step1
+  # using every other value
+  wl.step2 <- wl.step1[c(TRUE, FALSE)]
+  var.step2 <- var.step1[c(TRUE, FALSE)]
+  diff.wl.step2 <- diff(wl.step2)
+  diff.var.step2 <- diff(var.step2)
+  local.slope.step2 <- diff.var.step2 / diff.wl.step2
+  # using every fourth value
+  wl.step4 <- wl.step2[c(TRUE, FALSE)]
+  var.step4 <- var.step2[c(TRUE, FALSE)]
+  diff.wl.step4 <- diff(wl.step4)
+  diff.var.step4 <- diff(var.step4)
+  local.slope.step4 <- diff.var.step4 / diff.wl.step4
+  # using every eights value
+  wl.step8 <- wl.step4[c(TRUE, FALSE)]
+  # check var and wl deltas
+  # if TRUE we cannot discard intermediate skipped values
+  step1.test <- (abs(local.slope.step1) > delta.var.max) | (diff.wl.step1 > max.wl.step)
+  step2.test <- (abs(local.slope.step2) > delta.var.max) | (diff.wl.step2 > max.wl.step)
+  step4.test <- (abs(local.slope.step4) > delta.var.max) | (diff.wl.step4 > max.wl.step)
+  # decide wavelengths to drop
+  wls.to.keep <- unique(c(wl.step1[c(1, length(wl.step1))],
+                          wl.step1[extremes],
+                          wl.step1[-1][step1.test],
+                          wl.step2[-1][step2.test],
+                          wl.step4[-1][step4.test],
+                          wl.step8[TRUE]))
+  x[x[["w.length"]] %in% wls.to.keep, ]
 }
 
