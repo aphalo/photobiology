ae965ae9a64bf0c79f091c62d2a07fc01fee9d88
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2020-07-09 21:25:39 +0300

    Add test cases for new functions
    Fix "spct.idx" missing from all_spct_attr.ls and consequently was not being copied.

--- tests/testthat/test_attr_2tb.R
+++ tests/testthat/test_attr_2tb.R
@@ -53,6 +53,48 @@ test_that("tbNULL", {
 
 })
 
+test_that("spct_attributes", {
+
+  my.spct <- white_led.source_spct
+  tested.location <- data.frame(lon = 24.93545, lat = 60.16952)
+  setWhereMeasured(my.spct, tested.location)
+  tested.time <- getWhenMeasured(white_led.cps_spct)
+  tested.what <- getWhatMeasured(my.spct)
+  # tested.instr.desc <- getInstrDesc(my.spct)
+  # tested.instr.settings <- getInstrSettings(my.spct)
+  # normalization.flag <- is_normalized(my.spct)
+  # scaling.flag <- is_scaled(my.spct)
+
+  spct.l <- list(one = my.spct, two = my.spct)
+  my.mspct <- source_mspct(spct.l)
+  attributes.tb <- spct_metadata(my.mspct)
+
+  expect_named(my.mspct, c("one", "two"))
+  expect_is(attributes.tb[["spct.idx"]], "factor")
+  expect_equal(names(my.mspct), as.character(attributes.tb[["spct.idx"]]))
+
+  expect_equal(getWhenMeasured(my.mspct[["one"]])[1],
+               attributes.tb[["when.measured"]][1])
+  expect_equal(getWhenMeasured(my.mspct[["two"]])[1],
+               attributes.tb[["when.measured"]][2])
+
+  expect_equal(getWhatMeasured(my.mspct[["one"]])[1],
+               attributes.tb[["what.measured"]][1])
+  expect_equal(getWhatMeasured(my.mspct[["two"]])[1],
+               attributes.tb[["what.measured"]][2])
+
+  expect_equal(getWhereMeasured(my.mspct[["one"]])[["lon"]],
+               attributes.tb[["lon"]][1])
+  expect_equal(getWhereMeasured(my.mspct[["one"]])[["lat"]],
+               attributes.tb[["lat"]][1])
+
+  expect_equal(getWhereMeasured(my.mspct[["two"]])[["lon"]],
+               attributes.tb[["lon"]][2])
+  expect_equal(getWhereMeasured(my.mspct[["two"]])[["lat"]],
+               attributes.tb[["lat"]][2])
+
+})
+
 test_that("tb_irrad", {
 
   my.spct <- white_led.source_spct
---
+++ tests/testthat/test_drop_columns.R
@@ -0,0 +1,167 @@
+library("photobiology")
+
+context("drop_user_cols")
+
+test_that("source_spct", {
+
+  my.spct <- q2e(sun.spct, action = "add")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.e.irrad", "s.q.irrad", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.e.irrad", "s.q.irrad"))
+  expect_is(dropped.spct, "source_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.e.irrad, dropped.spct$s.e.irrad)
+  expect_equal(my.spct$s.q.irrad, dropped.spct$s.q.irrad)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  # energy
+  my.spct <- q2e(sun.spct, action = "replace")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.e.irrad", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.e.irrad"))
+  expect_is(dropped.spct, "source_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.e.irrad, dropped.spct$s.e.irrad)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  # photon
+  my.spct <- e2q(sun.spct, action = "replace")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.q.irrad", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.q.irrad"))
+  expect_is(dropped.spct, "source_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.q.irrad, dropped.spct$s.q.irrad)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+})
+
+test_that("response_spct", {
+
+  my.spct <- q2e(ccd.spct, action = "add")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.q.response", "s.e.response", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.q.response", "s.e.response"))
+  expect_is(dropped.spct, "response_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.e.response, dropped.spct$s.e.response)
+  expect_equal(my.spct$s.q.response, dropped.spct$s.q.response)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  # energy
+  my.spct <- q2e(ccd.spct, action = "replace")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.e.response", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.e.response"))
+  expect_is(dropped.spct, "response_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.e.response, dropped.spct$s.e.response)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  # photon
+  my.spct <- e2q(ccd.spct, action = "replace")
+  my.spct$A <- "A"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "s.q.response", "A", "two"))
+  expect_named(dropped.spct, c("w.length", "s.q.response"))
+  expect_is(dropped.spct, "response_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$s.q.response, dropped.spct$s.q.response)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+})
+
+test_that("filter_spct", {
+
+  my.spct <- T2A(polyester.spct, action = "add")
+  my.spct$Z <- "Z"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "Tfr", "A", "Z", "two"))
+  expect_named(dropped.spct, c("w.length", "Tfr", "A"))
+  expect_is(dropped.spct, "filter_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$Tfr, dropped.spct$Tfr)
+  expect_equal(my.spct$A, dropped.spct$A)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  my.spct <- T2A(polyester.spct, action = "replace")
+  my.spct$Z <- "Z"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "A", "Z", "two"))
+  expect_named(dropped.spct, c("w.length", "A"))
+  expect_is(dropped.spct, "filter_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$A, dropped.spct$A)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+  my.spct <- A2T(polyester.spct, action = "replace")
+  my.spct$Z <- "Z"
+  my.spct$two <- 2
+
+  dropped.spct <- drop_user_cols(my.spct)
+
+  expect_named(my.spct, c("w.length", "Tfr", "Z", "two"))
+  expect_named(dropped.spct, c("w.length", "Tfr"))
+  expect_is(dropped.spct, "filter_spct")
+  expect_equal(nrow(my.spct), nrow(dropped.spct))
+  expect_equal(my.spct$w.length, dropped.spct$w.length)
+  expect_equal(my.spct$Tfr, dropped.spct$Tfr)
+  # column 1 is "spct.idx"
+  expect_equal(spct_metadata(my.spct)[ , -1],
+               spct_metadata(dropped.spct)[ , -1])
+
+})
+
