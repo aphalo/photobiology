012e9bb7cb9eaf9f94c26e4e9ea08da7d76f8e63
Pedro Aphalo <pedro.aphalo@helsinki.fi>
2025-09-23 10:15:42 +0300

    Add tests for metadata in rbindspct()

--- tests/testthat/test_rbindspct.R
+++ tests/testthat/test_rbindspct.R
@@ -1,36 +1,178 @@
 library(photobiology)
 
-## need to add similar tests for additional classes
-## need to add additional methods
+## need to add tests for metadata
 
 context("binding")
 
+test_that("metadata with no normalization", {
+  # test shared code only once
+  # my.spct <- sun.spct
+  # my.mspct <- source_mspct(list(sun = my.spct, sun_1 = my.spct))
+  collection.mspct <- sun_evening.mspct
+  my.spct <- rbindspct(collection.mspct)
+
+
+  expect_equal(names(where_measured(my.spct, .bind.geocodes = FALSE)),
+               names(collection.mspct))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[1]],
+               where_measured(collection.mspct[[1]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[2]],
+               where_measured(collection.mspct[[2]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[3]],
+               where_measured(collection.mspct[[3]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[4]],
+               where_measured(collection.mspct[[4]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[5]],
+               where_measured(collection.mspct[[5]]))
+
+  expect_equal(names(when_measured(my.spct)), names(collection.mspct))
+  expect_equal(when_measured(my.spct)[[1]], when_measured(collection.mspct[[1]]))
+  expect_equal(when_measured(my.spct)[[2]], when_measured(collection.mspct[[2]]))
+  expect_equal(when_measured(my.spct)[[3]], when_measured(collection.mspct[[3]]))
+  expect_equal(when_measured(my.spct)[[4]], when_measured(collection.mspct[[4]]))
+  expect_equal(when_measured(my.spct)[[5]], when_measured(collection.mspct[[5]]))
+
+  expect_equal(names(what_measured(my.spct)), names(collection.mspct))
+  expect_equal(what_measured(my.spct)[[1]], what_measured(collection.mspct[[1]]))
+  expect_equal(what_measured(my.spct)[[2]], what_measured(collection.mspct[[2]]))
+  expect_equal(what_measured(my.spct)[[3]], what_measured(collection.mspct[[3]]))
+  expect_equal(what_measured(my.spct)[[4]], what_measured(collection.mspct[[4]]))
+  expect_equal(what_measured(my.spct)[[5]], what_measured(collection.mspct[[5]]))
+
+  expect_equal(names(how_measured(my.spct)), names(collection.mspct))
+  expect_equal(how_measured(my.spct)[[1]], how_measured(collection.mspct[[1]]))
+  expect_equal(how_measured(my.spct)[[2]], how_measured(collection.mspct[[2]]))
+  expect_equal(how_measured(my.spct)[[3]], how_measured(collection.mspct[[3]]))
+  expect_equal(how_measured(my.spct)[[4]], how_measured(collection.mspct[[4]]))
+  expect_equal(how_measured(my.spct)[[5]], how_measured(collection.mspct[[5]]))
+
+  expect_equal(names(instr_descriptor(my.spct)), names(collection.mspct))
+  expect_equal(instr_descriptor(my.spct)[[1]], instr_descriptor(collection.mspct[[1]]))
+  expect_equal(instr_descriptor(my.spct)[[2]], instr_descriptor(collection.mspct[[2]]))
+  expect_equal(instr_descriptor(my.spct)[[3]], instr_descriptor(collection.mspct[[3]]))
+  expect_equal(instr_descriptor(my.spct)[[4]], instr_descriptor(collection.mspct[[4]]))
+  expect_equal(instr_descriptor(my.spct)[[5]], instr_descriptor(collection.mspct[[5]]))
+
+  expect_equal(names(instr_settings(my.spct)), names(collection.mspct))
+  expect_equal(instr_settings(my.spct)[[1]], instr_settings(collection.mspct[[1]]))
+  expect_equal(instr_settings(my.spct)[[2]], instr_settings(collection.mspct[[2]]))
+  expect_equal(instr_settings(my.spct)[[3]], instr_settings(collection.mspct[[3]]))
+  expect_equal(instr_settings(my.spct)[[4]], instr_settings(collection.mspct[[4]]))
+  expect_equal(instr_settings(my.spct)[[5]], instr_settings(collection.mspct[[5]]))
+
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[1]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[2]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[3]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[4]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[5]]))
+
+})
+
+test_that("metadata with with normalization", {
+  # test shared code only once
+  # my.spct <- sun.spct
+  # my.mspct <- source_mspct(list(sun = my.spct, sun_1 = my.spct))
+  collection.mspct <- normalize(sun_evening.mspct)
+  my.spct <- rbindspct(collection.mspct)
+
+  expect_equal(names(where_measured(my.spct, .bind.geocodes = FALSE)),
+               names(collection.mspct))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[1]],
+               where_measured(collection.mspct[[1]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[2]],
+               where_measured(collection.mspct[[2]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[3]],
+               where_measured(collection.mspct[[3]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[4]],
+               where_measured(collection.mspct[[4]]))
+  expect_equal(where_measured(my.spct, .bind.geocodes = FALSE)[[5]],
+               where_measured(collection.mspct[[5]]))
+
+  expect_equal(names(when_measured(my.spct)), names(collection.mspct))
+  expect_equal(when_measured(my.spct)[[1]], when_measured(collection.mspct[[1]]))
+  expect_equal(when_measured(my.spct)[[2]], when_measured(collection.mspct[[2]]))
+  expect_equal(when_measured(my.spct)[[3]], when_measured(collection.mspct[[3]]))
+  expect_equal(when_measured(my.spct)[[4]], when_measured(collection.mspct[[4]]))
+  expect_equal(when_measured(my.spct)[[5]], when_measured(collection.mspct[[5]]))
+
+  expect_equal(names(what_measured(my.spct)), names(collection.mspct))
+  expect_equal(what_measured(my.spct)[[1]], what_measured(collection.mspct[[1]]))
+  expect_equal(what_measured(my.spct)[[2]], what_measured(collection.mspct[[2]]))
+  expect_equal(what_measured(my.spct)[[3]], what_measured(collection.mspct[[3]]))
+  expect_equal(what_measured(my.spct)[[4]], what_measured(collection.mspct[[4]]))
+  expect_equal(what_measured(my.spct)[[5]], what_measured(collection.mspct[[5]]))
+
+  expect_equal(names(how_measured(my.spct)), names(collection.mspct))
+  expect_equal(how_measured(my.spct)[[1]], how_measured(collection.mspct[[1]]))
+  expect_equal(how_measured(my.spct)[[2]], how_measured(collection.mspct[[2]]))
+  expect_equal(how_measured(my.spct)[[3]], how_measured(collection.mspct[[3]]))
+  expect_equal(how_measured(my.spct)[[4]], how_measured(collection.mspct[[4]]))
+  expect_equal(how_measured(my.spct)[[5]], how_measured(collection.mspct[[5]]))
+
+  expect_equal(names(instr_descriptor(my.spct)), names(collection.mspct))
+  expect_equal(instr_descriptor(my.spct)[[1]], instr_descriptor(collection.mspct[[1]]))
+  expect_equal(instr_descriptor(my.spct)[[2]], instr_descriptor(collection.mspct[[2]]))
+  expect_equal(instr_descriptor(my.spct)[[3]], instr_descriptor(collection.mspct[[3]]))
+  expect_equal(instr_descriptor(my.spct)[[4]], instr_descriptor(collection.mspct[[4]]))
+  expect_equal(instr_descriptor(my.spct)[[5]], instr_descriptor(collection.mspct[[5]]))
+
+  expect_equal(names(instr_settings(my.spct)), names(collection.mspct))
+  expect_equal(instr_settings(my.spct)[[1]], instr_settings(collection.mspct[[1]]))
+  expect_equal(instr_settings(my.spct)[[2]], instr_settings(collection.mspct[[2]]))
+  expect_equal(instr_settings(my.spct)[[3]], instr_settings(collection.mspct[[3]]))
+  expect_equal(instr_settings(my.spct)[[4]], instr_settings(collection.mspct[[4]]))
+  expect_equal(instr_settings(my.spct)[[5]], instr_settings(collection.mspct[[5]]))
+
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[1]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[2]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[3]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[4]]))
+  expect_equal(is_normalized(my.spct), is_normalized(collection.mspct[[5]]))
+
+  expect_equal(names(getNormalized(my.spct)), names(collection.mspct))
+  expect_equal(getNormalized(my.spct)[[1]], getNormalized(collection.mspct[[1]]))
+  expect_equal(getNormalized(my.spct)[[2]], getNormalized(collection.mspct[[2]]))
+  expect_equal(getNormalized(my.spct)[[3]], getNormalized(collection.mspct[[3]]))
+  expect_equal(getNormalized(my.spct)[[4]], getNormalized(collection.mspct[[4]]))
+  expect_equal(getNormalized(my.spct)[[5]], getNormalized(collection.mspct[[5]]))
+
+  expect_equal(names(getNormalized(my.spct)), names(collection.mspct))
+  expect_equal(getNormalized(my.spct)[[1]], getNormalized(collection.mspct[[1]]))
+  expect_equal(getNormalized(my.spct)[[2]], getNormalized(collection.mspct[[2]]))
+  expect_equal(getNormalized(my.spct)[[3]], getNormalized(collection.mspct[[3]]))
+  expect_equal(getNormalized(my.spct)[[4]], getNormalized(collection.mspct[[4]]))
+  expect_equal(getNormalized(my.spct)[[5]], getNormalized(collection.mspct[[5]]))
+
+})
+
+
 test_that("idfactor", {
   # test shared code only once
-  my.spct <- sun.spct
-  my.mspct <- source_mspct(list(sun = my.spct, sun_1 = my.spct))
+  # my.spct <- sun.spct
+  # my.mspct <- source_mspct(list(sun = my.spct, sun_1 = my.spct))
+  my.mspct <- e2q(sun_evening.mspct[1:2])
 
   expect_silent(long.spct <- rbindspct(my.mspct, idfactor = "test.id"))
   expect_named(long.spct, c("w.length", "s.e.irrad", "s.q.irrad", "test.id"))
-  expect_equal(levels(long.spct$test.id), c("sun", "sun_1"))
+  expect_equal(levels(long.spct$test.id), names(my.mspct))
   expect_equal(attr(long.spct, "idfactor"), "test.id")
   expect_equal(getIdFactor(long.spct), "test.id")
 
-  expect_silent(long.spct <- rbindspct(my.mspct["sun"], idfactor = "test.id"))
+  expect_silent(long.spct <- rbindspct(my.mspct[1], idfactor = "test.id"))
   expect_named(long.spct, c("w.length", "s.e.irrad", "s.q.irrad", "test.id"))
-  expect_equal(levels(long.spct$test.id), c("sun"))
+  expect_equal(levels(long.spct$test.id), names(my.mspct)[1])
   expect_equal(attr(long.spct, "idfactor"), "test.id")
   expect_equal(getIdFactor(long.spct), "test.id")
 
   expect_silent(long.spct <- rbindspct(my.mspct, idfactor = TRUE))
   expect_named(long.spct, c("w.length", "s.e.irrad", "s.q.irrad", "spct.idx"))
-  expect_equal(levels(long.spct$spct.idx), c("sun", "sun_1"))
+  expect_equal(levels(long.spct$spct.idx), names(my.mspct))
   expect_equal(attr(long.spct, "idfactor"), "spct.idx")
   expect_equal(getIdFactor(long.spct), "spct.idx")
 
-  expect_silent(long.spct <- rbindspct(my.mspct["sun"], idfactor = TRUE))
+  expect_silent(long.spct <- rbindspct(my.mspct[1], idfactor = TRUE))
   expect_named(long.spct, c("w.length", "s.e.irrad", "s.q.irrad", "spct.idx"))
-  expect_equal(levels(long.spct$spct.idx), c("sun"))
+  expect_equal(levels(long.spct$spct.idx), names(my.mspct)[1])
   expect_equal(attr(long.spct, "idfactor"), "spct.idx")
   expect_equal(getIdFactor(long.spct), "spct.idx")
 
@@ -39,7 +181,7 @@ test_that("idfactor", {
   expect_null(attr(long.spct, "idfactor"))
   expect_true(is.na(getIdFactor(long.spct)))
 
-  expect_silent(long.spct <- rbindspct(my.mspct["sun"], idfactor = FALSE))
+  expect_silent(long.spct <- rbindspct(my.mspct[1], idfactor = FALSE))
   expect_named(long.spct, c("w.length", "s.e.irrad", "s.q.irrad"))
   expect_null(attr(long.spct, "idfactor"))
   expect_true(is.na(getIdFactor(long.spct)))
